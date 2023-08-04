f26930b0fb22544fb5adaeed61686b365a1693b7
Jeshan1 <jectionjection@gmail.com>
2023-08-04 20:34:28 +0545

    title added

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ config('app.name')}}</title>
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>

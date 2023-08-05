dcc301c43230c849bc4d1b01e481971f4aa516d1
Jeshan1 <jectionjection@gmail.com>
2023-08-05 08:51:11 +0545

    add issue fix

--- resources/views/livewire/post-component.blade.php
+++ resources/views/livewire/post-component.blade.php
@@ -7,7 +7,7 @@
                 <div class="max-w-xl">
                 <section>
                     @if (session()->has('success'))
-                    <div class="relative flex flex-col sm:flex-row sm:items-center bg-gray-200 dark:bg-green-700 shadow rounded-md py-5 pl-6 pr-8 sm:pr-6 mb-3 mt-3" x-data="{ show: true }" x-init="setTimeout(() => { show = false },5000)" x-show="show">
+                    <div class="relative flex flex-col sm:flex-row sm:items-center bg-gray-200 dark:bg-green-700 shadow rounded-md py-5 pl-6 pr-8 sm:pr-6 mb-3 mt-3" x-data="{ show: true }" x-init="setTimeout(() => { show = false },1000)" x-show="show">
                         <div class="flex flex-row items-center border-b sm:border-b-0 w-full sm:w-auto pb-4 sm:pb-0">
                             <div class="text-green-500" dark:text-gray-500>
                                 <svg class="w-6 sm:w-5 h-6 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
